d688f5116692fa5a2c6eab978fff4e5bb18c4a3e
unknown <tecnologia@sergiosoriano.es>
2018-05-22 11:35:08 +0200

    añado bloque genesis

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "SorianoCoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001c08f8af8ae5f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210150c979713d4414ca35fad3f7b323edf07d8b0ae6dd325f64582d664e198fcf3f";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
